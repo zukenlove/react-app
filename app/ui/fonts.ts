61100893b36fd412980e5c275c95de461aab204c
zuken <nailofrantz@gmail.com>
2025-06-03 08:40:53 -0400

    great so fr

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,10 @@
-import { Lusitana } from 'next/font/google';
+import { Lusitana, Inter } from 'next/font/google';
 
 export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
+});
+
+export const inter = Inter({
+  subsets:['latin']
 });
